932b4d933e752a193c1971554cdfef829f621d08
codingarmy45 <135152967+codingarmy45@users.noreply.github.com>
2023-11-21 23:34:15 +0530

    Update HCI_GUI Assignment 3.py

--- HCI_GUI Assignment 3.py
+++ HCI_GUI Assignment 3.py
@@ -1,202 +1,68 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Import the tkinter module for GUI creation
+import tkinter as tk
 
-# In[1]:
+# Function to handle button clicks
+def on_button_click(value):
+    # Get the current content of the entry widget
+    current = entry.get()
+    # Clear the entry widget
+    entry.delete(0, tk.END)
+    # Insert the concatenation of the current content and the clicked button value
+    entry.insert(tk.END, str(current) + str(value))
 
+# Function to clear the entry widget
+def clear_entry():
+    # Delete the content of the entry widget
+    entry.delete(0, tk.END)
 
+# Function to evaluate and display the result
+def calculate_result():
+    try:
+        # Evaluate the expression entered in the entry widget
+        result = eval(entry.get())
+        # Clear the entry widget
+        entry.delete(0, tk.END)
+        # Display the result in the entry widget
+        entry.insert(tk.END, str(result))
+    except Exception as e:
+        # Handle exceptions (e.g., division by zero) by displaying an error message
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, "Error")
 
+# Create the main window
+root = tk.Tk()
+# Set the title of the window
+root.title("Calculator")
 
+# Create an entry widget for displaying and entering values
+entry = tk.Entry(root, width=30, borderwidth=5)
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-# In[ ]:
+# Define the labels for calculator buttons
+buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '=', '+'
+]
 
+# Initialize row and column values for grid placement
+row_val = 1
+col_val = 0
 
-from tkinter import *
-window=Tk()
-# add widgets here
+# Create and place buttons in a grid
+for button in buttons:
+    # Use lambda to pass the button value to the click function
+    tk.Button(root, text=button, padx=20, pady=20, command=lambda b=button: on_button_click(b) if b != '=' else calculate_result()).grid(row=row_val, column=col_val)
+    
+    # Increment column value, reset to 0 and increment row value if column exceeds 3
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
 
-window.title('Hello Python')
-window.geometry("300x200+10+20")
-window.mainloop()
+# Create a clear button that spans two columns
+tk.Button(root, text="Clear", padx=20, pady=20, command=clear_entry).grid(row=row_val, column=col_val, columnspan=2)
 
-
-# In[3]:
-
-
-
-
-
-# In[ ]:
-
-
-# Import Module
-from tkinter import *
-
-# create root window
-root = Tk()
-
-# root window title and dimension
-root.title("Welcome to LP1")
-# Set geometry (widthxheight)
-root.geometry('350x200')
-
-# all widgets will be here
-# Execute Tkinter
+# Run the main loop to display the GUI
 root.mainloop()
-
-
-# In[6]:
-
-
-# Import Module
-from tkinter import *
-
-# create root window
-root = Tk()
-
-# root window title and dimension
-root.title("Welcome to GeekForGeeks")
-# Set geometry(widthxheight)
-root.geometry('350x200')
-
-#adding a label to the root window
-lbl = Label(root, text = "Are you a Geek?")
-lbl.grid()
-
-# Execute Tkinter
-root.mainloop()
-
-
-# In[7]:
-
-
-# Import Module
-from tkinter import *
-
-# create root window
-root = Tk()
-
-# root window title and dimension
-root.title("Welcome to GeekForGeeks")
-# Set geometry(widthxheight)
-root.geometry('350x200')
-
-# adding a label to the root window
-lbl = Label(root, text = "Are you a Geek?")
-lbl.grid()
-
-# function to display text when
-# button is clicked
-def clicked():
-	lbl.configure(text = "I just got clicked")
-
-# button widget with red color text
-# inside
-btn = Button(root, text = "Click me" ,
-			fg = "red", command=clicked)
-# set Button grid
-btn.grid(column=1, row=0)
-
-# Execute Tkinter
-root.mainloop()
-
-
-# In[8]:
-
-
-# Import Module
-from tkinter import *
-
-# create root window
-root = Tk()
-
-# root window title and dimension
-root.title("Welcome to GeekForGeeks")
-# Set geometry(widthxheight)
-root.geometry('350x200')
-
-# adding a label to the root window
-lbl = Label(root, text = "Are you a Geek?")
-lbl.grid()
-
-# adding Entry Field
-txt = Entry(root, width=10)
-txt.grid(column =1, row =0)
-
-
-# function to display user text when
-# button is clicked
-def clicked():
-
-	res = "You wrote" + txt.get()
-	lbl.configure(text = res)
-
-# button widget with red color text inside
-btn = Button(root, text = "Click me" ,
-			fg = "red", command=clicked)
-# Set Button Grid
-btn.grid(column=2, row=0)
-
-# Execute Tkinter
-root.mainloop()
-
-
-# In[9]:
-
-
-# Import Module
-from tkinter import *
-
-# create root window
-root = Tk()
-
-# root window title and dimension
-root.title("Welcome to GeekForGeeks")
-# Set geometry(widthxheight)
-root.geometry('350x200')
-
-# adding menu bar in root window
-# new item in menu bar labelled as 'New'
-# adding more items in the menu bar
-menu = Menu(root)
-item = Menu(menu)
-item.add_command(label='New')
-menu.add_cascade(label='File', menu=item)
-root.config(menu=menu)
-
-# adding a label to the root window
-lbl = Label(root, text = "Are you a Geek?")
-lbl.grid()
-
-# adding Entry Field
-txt = Entry(root, width=10)
-txt.grid(column =1, row =0)
-
-
-# function to display user text when
-# button is clicked
-def clicked():
-
-	res = "You wrote" + txt.get()
-	lbl.configure(text = res)
-
-# button widget with red color text inside
-btn = Button(root, text = "Click me" ,
-			fg = "red", command=clicked)
-# Set Button Grid
-btn.grid(column=2, row=0)
-
-# Execute Tkinter
-root.mainloop()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
